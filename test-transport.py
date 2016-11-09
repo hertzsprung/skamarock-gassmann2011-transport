fc4270707327ec971d1414e0b8b19f7313c9a4c5
James Shaw <js102@zepler.net>
2016-11-09 21:26:35 +0000

    Add convergence test.
    Still uses centred differencing to give 2nd order convergence.  Order of
    convergence is found using a least squares fit.

--- test-transport.py
+++ test-transport.py
@@ -5,8 +5,16 @@ from transport import *
 def test_sine_wave_advection_with_baseline_accuracy():
     spec = SimulationSpec(InitialConditions.sine_wave())
     simulation = spec.advect()
-    simulation.dump("results")
     assert simulation.error_l2() < 0.65
 
 def test_third_order_convergence_on_uniform_mesh():
-    print("hello")
+    convergence = Convergence(SimulationSpec(InitialConditions.sine_wave()))
+
+    for i in range(6):
+        convergence.converge()
+
+    assert convergence.order() > 1.9 and convergence.order() < 2.1
+#    assert convergence.order() == pytest.approx(2, 0.1)
+
+
+    
--- transport.py
+++ transport.py
@@ -3,13 +3,15 @@ import numpy as np
 import os
 
 class SimulationSpec:
-    def __init__(self, initial):
+    def __init__(self, initial, dt = 0.05, nx = 10):
+        self._initial = initial
+        self._dt = dt
+        self._nx = nx
+
         self._width = 1.0
         self._end_time = 1.0
         self._u = 1
-        self._dt = 0.05
         self.times = np.arange(0, self._end_time+np.finfo(float).eps, step=self._dt)
-        self._nx = 10
         self.C = np.linspace(0.5*self._width/self._nx, 1-0.5*self._width/self._nx, num=self._nx)
         self._Cf = np.linspace(0, 1, num=self._nx+1)
         self.dx = [r - l for l,r in zip(self._Cf[:-1], self._Cf[1:])]
@@ -46,6 +48,15 @@ class SimulationSpec:
 
         return -self._u * (T_right - T_left) / self.dx[i]
 
+    def mean_dx(self):
+        return np.mean(self.dx)
+
+    def refine(self):
+        return SimulationSpec(self._initial, self._dt/2, self._nx*2)
+
+    def max_courant(self):
+        return np.max(self._u * self._dt / self._dx)
+
 class Simulation:
     def __init__(self, spec):
         self._spec = spec
@@ -68,6 +79,26 @@ class Simulation:
             with open(os.path.join(directory, str(self._spec.times[i])) + ".dat", 'w') as f:
                 for C, T_value in zip(self._spec.C, T):
                     print(C, T_value, file=f)
+
+class Convergence:
+    def __init__(self, spec):
+        self._spec = spec
+        self._dxs = []
+        self._errors_l2 = []
+
+    def converge(self):
+        simulation = self._spec.advect()
+        self._dxs.append(self._spec.mean_dx())
+        self._errors_l2.append(simulation.error_l2())
+        self._spec = self._spec.refine()
+
+    def order(self):
+        log_dxs = np.log(self._dxs)
+        log_errors_l2 = np.log(self._errors_l2)
+
+        A = np.vstack([log_dxs, np.ones(len(log_dxs))]).T
+        m, c = np.linalg.lstsq(A, log_errors_l2)[0]
+        return m
 
 class InitialConditions:
     @staticmethod
